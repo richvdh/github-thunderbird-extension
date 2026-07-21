ed536215dd5565c3ec77cee3d887e511d25b4ab1
Richard van der Hoff <richard@matrix.org>
2026-07-21 14:02:59 +0100

    auto-populate package version in manifest.json

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,27 @@
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+import fs from "node:fs";
+import path from "node:path";
+import pkg from "./package.json";
+
+/// Plugin to inject the package version into maniefst.json
+function injectVersion() {
+    return {
+        name: "inject-version",
+        buildStart() {
+            const root = this.environment._topLevelConfig.root;
+            const outPath = path.resolve(root, "public/manifest.json");
+            const template = JSON.parse(
+                fs.readFileSync(
+                    path.resolve(root, "manifest.template.json"),
+                    "utf-8",
+                ),
+            );
+            template.version = pkg.version;
+            fs.writeFileSync(outPath, JSON.stringify(template, null, 2));
+        },
+    };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,4 +50,5 @@ export default defineConfig({
     test: {
         include: ["../test/**/*.test.ts"],
     },
+    plugins: [injectVersion()],
 });
